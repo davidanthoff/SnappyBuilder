e28f0fe68837edb1f3fafc98efa82236f3918f5f
David Anthoff <anthoff@berkeley.edu>
2017-11-07 17:38:05 -0800

    Add build_tarballs.jl

---
+++ build_tarballs.jl
@@ -0,0 +1,33 @@
+using BinaryBuilder
+
+platforms = [
+  BinaryProvider.Linux(:i686, :glibc),
+  BinaryProvider.Linux(:x86_64, :glibc),
+  BinaryProvider.Linux(:aarch64, :glibc),
+  BinaryProvider.Linux(:armv7l, :glibc),
+  BinaryProvider.Linux(:powerpc64le, :glibc),
+  BinaryProvider.Windows(:i686),
+  BinaryProvider.Windows(:x86_64)
+]
+
+sources = [
+  "https://github.com/google/snappy/archive/1.1.7.tar.gz" =>
+"3dfa02e873ff51a11ee02b9ca391807f0c8ea0529a4924afa645fbf97163f9d4",
+]
+
+script = raw"""
+cd $WORKSPACE/srcdir
+cd snappy-1.1.7
+mkdir build
+cd build
+cmake ../ -DBUILD_SHARED_LIBS=On
+make
+make install
+
+"""
+
+products = prefix -> [
+	LibraryProduct(prefix,"libsnappy")
+]
+
+autobuild(pwd(), "SnappyBuilder", platforms, sources, script, products)
